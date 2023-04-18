d2052ed5b568a306cb9a12f3c630bdc2fd801041
Felipe Torrezan <felipe.torrezan@iar.com>
2023-04-18 17:40:22 +0200

    Added map tail function

--- examples/arm/iar-functions.cmake
+++ examples/arm/iar-functions.cmake
@@ -14,3 +14,18 @@ endfunction()
 function(iar_elf_tool_bin TARGET)
   add_custom_command(TARGET ${TARGET} POST_BUILD COMMAND ${CMAKE_IAR_ELFTOOL} --silent --bin $<TARGET_FILE:${TARGET}> $<IF:$<BOOL:$<TARGET_PROPERTY:${TARGET},OUTPUT_NAME>>,$<TARGET_PROPERTY:${TARGET},OUTPUT_NAME>,$<TARGET_PROPERTY:${TARGET},NAME>>.bin)
 endfunction()
+
+# Display the resource usage from the summary in the map file at executable POST_BUILD
+function(iar_linker_summary TARGET)
+  find_program(TAIL_COMMAND
+               NAMES tail
+               REQUIRED)
+  get_target_property(target_type ${TARGET} TYPE)
+  if (target_type STREQUAL "EXECUTABLE")
+    add_custom_command(TARGET ${TARGET} POST_BUILD COMMAND ${CMAKE_COMMAND} -E echo "-------------------------------------------------")
+    add_custom_command(TARGET ${TARGET} POST_BUILD COMMAND ${TAIL_COMMAND} -n6 $<TARGET_PROPERTY:NAME>.map)
+    add_custom_command(TARGET ${TARGET} POST_BUILD COMMAND ${CMAKE_COMMAND} -E echo "-------------------------------------------------")
+  else()
+    add_custom_command(TARGET ${TARGET} POST_BUILD COMMAND ${CMAKE_COMMAND} -E echo "iar_summary: this option is only available for executable targets.")
+  endif()
+endfunction()
