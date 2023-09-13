2355d1b617ecbe9d853de716b862721a9c566d33
Felipe Torrezan <felipe.torrezan@iar.com>
2023-09-13 19:42:01 +0200

    Update tutorial

--- examples/iar-toolchain.cmake
+++ examples/iar-toolchain.cmake
@@ -1,15 +1,22 @@
 # Toolchain File for the IAR C/C++ Compiler
 
-# Action: Set the `TOOLKIT` variable
-# Examples: arm, riscv, rh850, rl78, rx, stm8, 430, 8051, avr or v850
-# Alternative: override the default TOOLKIT_DIR (/path/to/installation/<arch>)
+# Set CMake for cross-compiling
+set(CMAKE_SYSTEM_NAME Generic)
+
+# `TOOLKIT` variable
+#  Used by find_program() for compiler detection.
+#
+#  When the installation can be found in a default install location, use the target architecture
+#  - examples: arm (*default), riscv, rh850, rl78, rx, stm8, 430, 8051, avr or v850
+#
+#  For *custom* install locations, set it to the TOOLKIT_DIR for the target architecture
+#  - example: 
+#    - Tools installed at D:/IAR/ARM/9.40.1 needs to
+#    - set(TOOLKIT "D:/IAR/ARM/9.40.1/arm")
 set(TOOLKIT arm)
 
 # Get the toolchain target from the TOOLKIT
 get_filename_component(CMAKE_SYSTEM_PROCESSOR ${TOOLKIT} NAME)
-
-# Set CMake for cross-compiling
-set(CMAKE_SYSTEM_NAME Generic)
 
 # IAR C Compiler
 find_program(CMAKE_C_COMPILER
@@ -27,7 +34,7 @@ find_program(CMAKE_CXX_COMPILER
   PATHS ${TOOLKIT}
         "$ENV{PROGRAMFILES}/IAR Systems/*"
         "$ENV{ProgramFiles\(x86\)}/IAR Systems/*"
-        /opt/iarsystems/bx${CMAKE_SYSTEM_PROCESSOR}
+        /opt/iarsystems/bx${CMAKE_SYSTEM_PROCESSOR}-
   PATH_SUFFIXES bin ${CMAKE_SYSTEM_PROCESSOR}/bin
   REQUIRED )
 
@@ -48,4 +55,3 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 get_filename_component(BIN_DIR ${CMAKE_C_COMPILER} DIRECTORY)
 get_filename_component(TOOLKIT_DIR ${BIN_DIR} PATH)
 unset(BIN_DIR)
-
